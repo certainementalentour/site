43488789be0a40cc42558ccfd7baa7b2152629f7
certainementalentour <mael.ballais@icloud.com>
2025-02-01 18:50:16 +0100

    ajout des fonctions de gestion des cookies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # site  
 
-**[entrer sur le site](site/index.html)**  
+#**[entrer sur le site](site/index.html)**  
 ![image très intéressante et bien dessinée](site/ressources/favicon/android-icon-192x192.png "titre")  
 ```
 merci de remplacer cette image par autre chose que du picasso
@@ -10,8 +10,10 @@ contribue pour espérer un café
 ## à implémenter : 
 	1. page de `minuteurs` pour faire son café en fonction de divers paramètres  
 	2.  
-	3. 
-
+	3.  
+  
+## notes :
+* !#f03c15 essayez au maximum de faire les pop-up en css afin de ne pas se faire bloquer par les navigateurs intelligents
 
 ## plan  
 
--- site/ressources/scripts/cookies.js
+++ site/ressources/scripts/cookies.js
@@ -1,6 +1,44 @@
-let valide = false;
-if (valide){
-	pass;
-} else {
-	//faire les 12 travaux d'hercule
+function obtenirValeurcookie(cookieName) {
+	const name = cookieName + "=";
+	const decodedCookie = decodeURIComponent(document.cookie);
+	const cookieArray = decodedCookie.split('; ');
+  
+	for (let i = 0; i < cookieArray.length; i++) {
+	  if (cookieArray[i].indexOf(name) === 0) {
+		return cookieArray[i].substring(name.length);
+	  }
+	}
+	return null; // Si le cookie n'existe pas
 }
+
+function depotCookie(cookieName, value, daysToLive, options) {
+	//depotCookie('cle', 'valeur', int, { path: '/', domain: 'example.tld', secure: true });
+	let expires = ""
+	if (daysToLive) {
+		let date = new Date();
+		date.setTime(date.getTime() + (daysToLive * 24 * 60 *1000));
+		expires = "; expires=" + date.toUTCString();
+	} else {
+		expires = "";
+	}
+	let cookie = cookieName + "=" + (value || "") + expires;
+	if (options) {
+		for (let key in options) {
+			cookie += `; ${key}=${options[key]}`;
+		}
+	}
+	document.cookie = cookie;
+} function retirerCookie(cookieName) {depotCookie(cookieName, "", -1);}
+
+function verificationConfientialite() {
+	const valeurCookie = obtenirValeurcookie('validationConfidentialite');
+	if (valeurCookie) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+//let valide = false;
+//depotCookie('validationConfidentialite', valide, 5);
